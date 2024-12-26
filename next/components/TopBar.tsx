8e20112daaf5b1435ffde6d12f44df4853353478
Sam Q <sam@squail.org>
2024-12-26 00:05:55 +0000

    feat: created template

---
+++ next/components/TopBar.tsx
@@ -0,0 +1,9 @@
+const TopBar = () => {
+    return (
+        <div className="flex flex-row justify-center mt-4">
+            <h1 className="text-[#16a34a] text-6xl font-serif">Bolna</h1>
+        </div>
+    )
+}
+
+export default TopBar;
